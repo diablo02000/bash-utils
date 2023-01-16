32d7d1244e8329a0189a2ee21827d506ca6af82b
Christophe Biguereau <christophe.biguereau@manomano.com>
2023-01-16 18:30:44 +0100

    fix: Parsing when credential is not define.

--- utils.sh
+++ utils.sh
@@ -7,7 +7,7 @@ function join {
 }
 
 # Parse any urls and return:
-# - protocole
+# - scheme
 # - credential
 # - host
 # - port
@@ -16,19 +16,26 @@ function join {
 function parse_url(){
   local _url="${1}"
   local -n _output_var="${2}"
+  local _scheme _cred _host_port
 
-  # Extract proto from url
-  read -r _proto _url <<< "$(sed -e's,^\(.*://\)\(.*\),\1 \2,g' <<< "${_url}")"
+  # Extract scheme from url
+  _scheme="$(sed -e 's,^\(.*://\).*,\1,g' <<< "${_url}")"
+  _url="${_url/$_scheme}" # Remove scheme from url
+  _scheme="${_scheme/:\/\/}" # Remove :// from scheme
 
-  # Extact cred if any from url
-  read -r _cred _url <<< "$(sed -e's,^\(.*@\)\(.*\),\1 \2,g' <<< "${_url}")"
+  # Extact credential if present from url
+  if grep -Eq '@[a-zA-Z]' <<< "${_url}";
+  then
+    IFS='@' read -r _cred _url <<< "${_url}"
+  fi
 
   # Extract host and port from url
-  read -r _host_port _url <<< "$(sed -e's,^\(.*:[0-9]*\)\(.*\),\1 \2,g' <<< "${_url}")"
+  _host_port="$(cut -d/ -f1  <<< "${_url}")"
+  _url="${_url/$_host_port}" # Remove host and port pattern from url.
 
   # Split host and port
   IFS=':' read -r _host _port <<< "${_host_port}"
 
   # Return declarative array
-  _output_var=([proto]="${_proto}" [credential]="${_cred/@}" [host]="${_host}" [port]="${_port}" [uri]="${_url}")
+  _output_var=([scheme]="${_scheme}" [credential]="${_cred}" [host]="${_host}" [port]="${_port}" [uri]="${_url}")
 }
